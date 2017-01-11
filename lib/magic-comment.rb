f805072496d98fa24a3cb731dedf57acf16fa65b
Lohn IMAI <imai@has-key.org>
2017-01-11 19:45:18 +0900

    commit

--- lib/magic-comment.rb
+++ lib/magic-comment.rb
@@ -2,47 +2,32 @@
 # A simple library to prepend magic comments for encoding to multiple ".rb" files
 require 'pathname'
 
-module MagicComment
+class MagicComment
+  attr_accessor :paths, :filelist, :encoding, :template, :extensions, :magic_comment
 
-  # Options :
-  # - paths
-  def self.process(*paths)
-    # defaults
-    paths.flatten!
-    paths.push Dir.pwd if paths.empty?
+  # options:
+  #   extensions: additional extensions
+  #   encoding: difference encoding if you want
+  def initialize(*args, options = {})
+    self.paths = args.empty? ? [Dir.pwd] : args.flatten
 
-    encoding = "coding: utf-8"
-    default_comment = "# {encoding}\n"
+    self.encoding = options[:encoding] || 'utf-8'
+    self.template= "# coding: {encoding}\n"
 
-    # TODO : add options for recursivity (and application of the script to a single file)
-    extensions = {
-      '.rb' => default_comment,
-      '.rake' => default_comment,
-      '.haml' => "-#{default_comment}",
-    }
+    self.extensions = %w(.rb .rake)
+    self.extensions += options[:extensions] if options[:extensions].instance_of? Array
 
-    files = []
-    paths.each do |path|
-      path = Pathname.new path
-      if path.file?
-        files.push path
-      elsif path.directory?
-        files += Dir.glob path.join('**', "*{#{extensions.keys.join ','}}")
-      end
-    end
+    generate_filelist(paths)
+    self.magic_comment = template.sub '{encoding}', encoding
+  end
 
-    default_comment.sub! '{encoding}', encoding
-    extensions.each_key do |key|
-      extensions[key].sub! '{encoding}', encoding
-    end
-
-    files.each do |filename|
+  def run!
+    filelist.each do |filename|
       file = File.new(filename, "r+")
-      magic_comment = extensions[ File.extname(file) ] || default_comment
       lines = file.readlines
 
       # remove current encoding comment(s)
-      while lines[0].match(/^-?# ?(-\*-)? ?(en)?coding/)
+      while lines[0].match(/)?coding/)
         lines.shift
       end
 
@@ -56,7 +41,28 @@ module MagicComment
       file.truncate(body.bytesize)
       file.close
     end
-
     puts "Magic comments set for #{files.length} source files"
+  end
+
+  private
+
+  def generate_filelist(*paths)
+    self.filelist = paths.each_with_object([]) do |path, files|
+      path = Pathname.new path
+      if path.file?
+        files.push path
+      elsif path.directory?
+        files += Dir.glob path.join('**', "*{#{extensions.join ','}}")
+      end
+    end
+  end
+
+  def substitution(obj)
+    if obj.instance_of? Hash
+      obj.each_with_object do |(key, value), hash|
+        hash[key] = substitution(value)
+      end
+    else
+    end
   end
 end
